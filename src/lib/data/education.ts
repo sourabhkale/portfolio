a5cb55aea2cbc5619b764e8a743ee569eeb2df8a
Sourabh Kale <sourabhkale@outlook.com>
2024-10-28 20:21:25 +0530

    updated education

--- src/lib/data/education.ts
+++ src/lib/data/education.ts
@@ -15,17 +15,17 @@ export const items: Array<Education> = [
 		subjects: ['C', 'Algorithm', 'Algebra', 'Python', 'C++', 'Java']
 	},
 	{
-		degree: 'PhD of Computer Science',
+		degree: 'Bachelor of Science in computer science',
 		description: '',
-		location: 'USA',
+		location: 'Kolhapur',
 		logo: Assets.Unknown,
 		name: '',
-		organization: 'MIT',
-		period: { from: new Date(2023, 0, 1) },
+		organization: 'Shivaji university',
+		period: { from: new Date(2016, 0, 1), to: new Date(2020, 5, 1) },
 		shortDescription: '',
-		slug: 'dummy-education-item-2',
-		subjects: ['Assembly', 'Rust', 'Computer Architecture', 'Algorithms and Data structures']
-	}
+		slug: 'dummy-education-item-1',
+		subjects: ['C', 'Algorithm']
+	},
 ];
 
 export const title = 'Education';
